33472b5555b0e28cd4b5b9b08aa92ef28118065c
william-b-henderson <william_henderson@berkeley.edu>
2021-10-14 00:01:32 -0700

    Built main functionality for javascript calculator. Need to fix bugs where you can backspace an = output, and where typing after an = and pressing = again returns the former value

--- projects/project2/calculator_hints.js
+++ projects/project2/calculator_hints.js
@@ -11,29 +11,39 @@ Hint: We need:
       (2) the string value that is shown on the display screen
       (3) the operator (+, x, -, and ÷) that is selected.  */
       
-      let total = ________;
-      let strbuffer = ________;
-      let operator = _______;
+      let total = 0;
+      let strbuffer = "0";
+      let operator = "";
       
       /*  FUNC DESCRIPTION: Operator calculations. Create the in +, x, -, and ÷ operator calculations. The plus operator is done for you!
           Uncomment and fill in the blank spaces. */
       function calculations() {
-          const intBuffer = _________; // Hint: Use parseInt to convert string to integer
+          const intBuffer = parseInt(strbuffer); // Hint: Use parseInt to convert string to integer
           if (operator === "+") {
               total += intBuffer;
           }
+          else if (operator === "-") {
+              total -= intBuffer;
+          }
+          else if (operator === "x") {
+            total *= intBuffer;
+          }
+          else if (operator === "÷") {
+              total = total / intBuffer;
+          }
+          
           //ADD THE OTHER OPERATORS
       }
       
       /*   FUNC DESCRIPTION: If user input is a number, create the function. */
       function makesNumber(value) {
           if (strbuffer === "0") {
-              strbuffer = _______;
+              strbuffer = value;
           } else {
           /*  If strbuffer is not 0, meaning there is a previous number typed in already, what should we display on the screen?
           Hint: How do we concatenate strings? If you are stuck, imagine typing in a "5" into the calculator, making strbuffer into "5". 
           Then imagine typing "3" into the calculator. Now "3" is value and strbuffer is still at "5", so strbuffer will now be 53.  */
-              ____________________
+              strbuffer += value;
           }
       }
       
@@ -43,40 +53,51 @@ Hint: We need:
           //make functionality for symbol C
           //make functionality for symbol ← Hint: .substring might be helpful! 
           //make functionality for symbol = Hint: use operator variable. Also call a function we created already!
-          ...
-          else { //make functionality if symbol is an operator
+          //...
+          //else { //make functionality if symbol is an operator
           const intBuffer = parseInt(strbuffer);
           if (total === 0) {
-              ____________;
+              return;
           } else {
-              ____________;
+              calculations();
           }
-          operator = ______;
-          strbuffer = _____;
+          operator = symbol;
+          strbuffer = total;
           }
-      }
+      //}
       
       /*  FUNC DESCRIPTION: Write the function to set listeners. This is how we will make the HTML interactive with the JS!
           This is where we sense when a user clicks a certain button and send this information to our buttonClicked function. */
       function setListeners() {
       //Hint: We want to select all buttons from html and make it so that something happens when you click on the buttons! querySelectorAll might be helpful
-          let ______ = document.________(________); 
-          for (item of ________) {
+          let buttons = document.querySelectorAll("button"); 
+          for (item of buttons) {
+              item.addEventListener("click", buttonClicked(item.target.innerText));
           //Hint: addEventListener might be useful.
           //Hint: event.target.innerText might be helpful. innerText return type is a string
           }
       }
       
       //Make sure to call setListeners!!!
-      ________________________
+      setListeners();
       
       /*  FUNC DESCRIPTION: Now we will write the function that takes care of when a button is clicked. */
       function buttonClicked(valueClicked) {
           if (isNaN(parseInt(valueClicked))) { //NaN means "Not a Number"
               //Hint: call a function we just created!
+              makesSymbol(valueClicked);
           } else {
               //Hint: call a function we just created!
+              makesNumber(valueClicked);
           }
-          ______________________ = ___________;
+          setDisplay(strbuffer);
       // Hint: we need to change what number appears on the screen! to change html, one listener you could use is querySelector
+      }
+
+      /**
+       * Sets the display area of the calculator to show displayString.
+       * @param {*} displayString : The string that is going to be displayed
+       */
+      function setDisplay(displayString) {
+        document.querySelector("section").target.innerText = displayString; 
       }
